6c00d046fd9cad00bab71eaf955379c87d3e39d2
DouglasDL28 <dele18037@uvg.edu.gt>
2019-03-25 11:55:08 -0600

    Cree las clases.

---
+++ src/com/company/Patient.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Patient {
+    String nombre;
+    String sintoma;
+    String codigo;
+
+    public Patient(String nombre, String sintoma, String codigo){
+        this.nombre = nombre;
+        this.sintoma = sintoma;
+        this.codigo = codigo;
+    }
+}
